de728b86424af624c6844d30cd24ba91043845c4
Terence Lee <hone02@gmail.com>
2014-08-12 09:16:57 -0700

    it just uses ruby everywhere

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -11,3 +11,4 @@ setup_audio("#whoops-btn", "#whoops-audio");
 setup_audio("#it-worked-btn", "#it-worked-audio");
 setup_audio("#not-doing-btn", "#not-doing-audio");
 setup_audio("#not-a-lot-of-work-btn", "#not-a-lot-of-work-audio");
+setup_audio("#ruby-everywhere-btn", "#ruby-everywhere-audio");
